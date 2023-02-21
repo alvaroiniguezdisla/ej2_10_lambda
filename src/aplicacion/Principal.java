63c6522a36e4e1525de624a23fc7b6fbc2163f02
Álvaro <ainiguezdislaa@gmail.com>
2023-02-21 17:14:06 +0100

    ej terminado

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,2 +1,12 @@
-package aplicacion;public class Principal {
+package aplicacion;
+import java.util.Scanner;
+import dominio.ej2_10_lambda;
+public class Principal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un número: ");
+        int n = sc.nextInt();
+        System.out.println("Los números pares hasta " + n + " son: " + ej2_10_lambda.listapares(n));
+    }
+
 }
--- src/dominio/ej2_10_lambda.java
+++ src/dominio/ej2_10_lambda.java
@@ -1,2 +1,10 @@
-package dominio;public class ej2_10_lambda {
+package dominio;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.ArrayList;
+
+public class ej2_10_lambda {
+    public static ArrayList listapares(int n){
+        return Stream.iterate(0,i->i+1).limit(n).filter(i->i%2==0).collect(Collectors.toCollection(ArrayList::new));
+    }
 }
